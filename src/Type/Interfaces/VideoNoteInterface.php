19d704eca481975664eb143d4c2c4531cb823da6
polkA767 <yudov.nikita@bk.ru>
2024-01-08 01:22:48 +0300

    добавил несколько интерфейсов src/Type

--- src/Type/Interfaces/VideoNoteInterface.php
+++ src/Type/Interfaces/VideoNoteInterface.php
@@ -2,8 +2,52 @@
 declare(strict_types=1);
 
 namespace PHPTCloud\TelegramApi\Type\Interfaces;
+/**
+ * @author  Юдов Алексей tcloud.ax@gmail.com
+ * @version 1.0.0
+ *
+ * Этот объект представляет собой видеосообщение (доступно в приложениях Telegram начиная с версии 4.0).
+ * @link    https://telegram.org/blog/video-messages-and-telescope
+ * @link    https://core.telegram.org/bots/api#videonote
+ */
 
 interface VideoNoteInterface
 {
+    /**
+     * Идентификатор этого файла, который можно использовать для загрузки или повторного использования файла.
+     *
+     * @return string
+     */
+    public function getFileId(): string;
 
+    /**
+     * Уникальный идентификатор этого файла, который должен быть одинаковым во времени и для разных ботов.
+     * Невозможно использовать для загрузки или повторного использования файла.
+     *
+     * @return string
+     */
+    public function getFileUniqueId(): string;
+
+    /**
+     * Ширина и высота видео (диаметр видеосообщения), определяемые отправителем.
+     *
+     * @return int
+     */
+    public function getLength(): int;
+
+    /**
+     * Продолжительность видео в секундах, указанная отправителем.
+     *
+     * @return int
+     */
+    public function getDuration(): int;
+
+    /**
+     * Необязательный. Миниатюра видео
+     *
+     * @return PhotoSizeInterface|null
+     */
+    public function getThumbnail(): ?PhotoSizeInterface;
+
+    
 }
--- src/Type/Interfaces/WebAppInfoInterface.php
+++ src/Type/Interfaces/WebAppInfoInterface.php
@@ -3,7 +3,22 @@ declare(strict_types=1);
 
 namespace PHPTCloud\TelegramApi\Type\Interfaces;
 
+/**
+ * @author  Юдов Алексей tcloud.ax@gmail.com
+ * @version 1.0.0
+ *
+ * Описывает веб-приложение.
+ * @link    https://core.telegram.org/bots/webapps
+ * @link    https://core.telegram.org/bots/api#webappinfo
+ */
 interface WebAppInfoInterface
 {
-
+    /**
+     * URL-адрес HTTPS веб-приложения, который необходимо открыть с дополнительными данными, как указано в
+     * разделе «Инициализация веб-приложений».
+     * @link    https://core.telegram.org/bots/webapps#initializing-mini-apps
+     *
+     * @return string
+     */
+    public function getUrl(): string;
 }
