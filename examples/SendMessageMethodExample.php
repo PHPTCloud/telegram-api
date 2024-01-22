ba31aa16c220fc14a6bba6036dc46b1578458a52
--global <tcloud.ax@gmail.com>
2024-01-22 14:17:56 +0300

    Изменил создание сервисов в TelegramApiManager

--- examples/SendMessageMethodExample.php
+++ examples/SendMessageMethodExample.php
@@ -1,7 +1,8 @@
 <?php
+
 declare(strict_types=1);
 
-require(__DIR__ . '/../vendor/autoload.php');
+require __DIR__ . '/../vendor/autoload.php';
 
 // Загружаем переменные из .env
 $dotenv = new Symfony\Component\Dotenv\Dotenv();
@@ -11,34 +12,40 @@ $token = $_ENV['TELEGRAM_BOT_TOKEN'];
 $chatId = $_ENV['TELEGRAM_CHAT_ID'];
 
 // Инициализируем менеджер для интеграции с Telegram API
-$manager = \PHPTCloud\TelegramApi\TelegramApiManagerFactory::create($token);
+$manager = PHPTCloud\TelegramApi\TelegramApiManagerFactory::create($token);
 
 // Для создания простого текстового сообщения можно не использовать MessageArgumentBuilder. Однако, что
 // бы соблюдать уровень сцепления модулей рекомендую не привязываться к конкретным реализациям и исполь
 // зовать билдеры и фабрики.
-$messageBuilder = new \PHPTCloud\TelegramApi\Argument\Builder\MessageArgumentBuilder();
+$messageBuilder = new PHPTCloud\TelegramApi\Argument\Builder\MessageArgumentBuilder();
 $message = $messageBuilder->setChatId($chatId)
     ->setText('Простое текстовое сообщение.')
-    ->build();
+    ->build()
+;
 
 $result = $manager->sendMessage($message); // @return \PHPTCloud\TelegramApi\Type\Interfaces\MessageInterface
 dump($result);
 
 // Также можно указать parse_mode параметр для использования форматирования текста.
 $message = $messageBuilder->setChatId($chatId)
-    ->setText('*Жирный текст*, _курсив_, [ссылка](http://www.example.com/) и прочее...')
-    ->setParseMode(\PHPTCloud\TelegramApi\FormattingLanguagesEnum::MARKDOWN->value)
+    ->setText('*Жирный текст*, _курсив_, [ссылка](https://www.example.com/) и прочее...')
+    ->setParseMode(PHPTCloud\TelegramApi\FormattingLanguagesEnum::MARKDOWN->value)
     // ->setNotificationDisabled(true)
     // ->setContentProtected(true)
     // ->setMessageThreadId(1)
-    ->build();
+    ->build()
+;
+
+$result = $manager->sendMessage($message); // @return \PHPTCloud\TelegramApi\Type\Interfaces\MessageInterface
+dump($result);
 
 // Пример отправки кода. В объекте Code автоматически происходит форматирование, но если вам не подходит этот
 // метод, то можно отправить "сырой" отформатированный заранее контент в sendMessage.
 $message = $messageBuilder->setChatId($chatId)
-    ->setText((string)(new \PHPTCloud\TelegramApi\Type\ValueObject\Code('$foo = "Код на PHP";', 'php')))
-    ->setParseMode(\PHPTCloud\TelegramApi\FormattingLanguagesEnum::MARKDOWN->value)
-    ->build();
+    ->setText((string) (new PHPTCloud\TelegramApi\Type\ValueObject\Code('$foo = "Код на PHP";', 'php')))
+    ->setParseMode(PHPTCloud\TelegramApi\FormattingLanguagesEnum::MARKDOWN->value)
+    ->build()
+;
 
 $result = $manager->sendMessage($message); // @return \PHPTCloud\TelegramApi\Type\Interfaces\MessageInterface
 dump($result);
@@ -47,12 +54,13 @@ dump($result);
 // @link https://core.telegram.org/bots/api#messageentity
 $message = $messageBuilder->setChatId($chatId)
     ->setText('Жирный текст, обычный текст.')
-    ->addEntity(new \PHPTCloud\TelegramApi\Argument\DataObject\MessageEntityArgument(
-        \PHPTCloud\TelegramApi\Type\Enums\MessageEntityTypeEnum::BOLD_TEXT->value,
+    ->addEntity(new PHPTCloud\TelegramApi\Argument\DataObject\MessageEntityArgument(
+        PHPTCloud\TelegramApi\Type\Enums\MessageEntityTypeEnum::BOLD_TEXT->value,
         0,
         12,
     ))
-    ->build();
+    ->build()
+;
 
 $result = $manager->sendMessage($message); // @return \PHPTCloud\TelegramApi\Type\Interfaces\MessageInterface
 dump($result);
@@ -62,11 +70,12 @@ dump($result);
 $message = $messageBuilder->setChatId($chatId)
     ->setText('LinkPreviewOptions - https://core.telegram.org/bots/api#linkpreviewoptions')
     ->setLinkPreviewOptions(
-        new \PHPTCloud\TelegramApi\Argument\DataObject\LinkPreviewOptionsArgument(
+        new PHPTCloud\TelegramApi\Argument\DataObject\LinkPreviewOptionsArgument(
             true, // убираем превью у ссылки.
         ),
     )
-    ->build();
+    ->build()
+;
 
 $result = $manager->sendMessage($message); // @return \PHPTCloud\TelegramApi\Type\Interfaces\MessageInterface
 dump($result);
--- src/TelegramApiManager.php
+++ src/TelegramApiManager.php
@@ -9,6 +9,9 @@ use PHPTCloud\TelegramApi\Argument\Interfaces\DataObject\MessageArgumentInterfac
 use PHPTCloud\TelegramApi\DomainService\Interfaces\Factory\ChatDomainServiceFactoryInterface;
 use PHPTCloud\TelegramApi\DomainService\Interfaces\Factory\MessageDomainServiceFactoryInterface;
 use PHPTCloud\TelegramApi\DomainService\Interfaces\Factory\TelegramBotDomainServiceFactoryInterface;
+use PHPTCloud\TelegramApi\DomainService\Interfaces\Service\ChatDomainServiceInterface;
+use PHPTCloud\TelegramApi\DomainService\Interfaces\Service\MessageDomainServiceInterface;
+use PHPTCloud\TelegramApi\DomainService\Interfaces\Service\TelegramBotDomainServiceInterface;
 use PHPTCloud\TelegramApi\Type\Interfaces\DataObject\ChatInterface;
 use PHPTCloud\TelegramApi\Type\Interfaces\DataObject\MessageInterface;
 use PHPTCloud\TelegramApi\Type\Interfaces\DataObject\UserInterface;
@@ -21,6 +24,10 @@ use PHPTCloud\TelegramApi\Type\Interfaces\DataObject\UserInterface;
 class TelegramApiManager implements TelegramApiManagerInterface
 {
     private ?string $host = self::TELEGRAM_API_HOST;
+
+    private ?MessageDomainServiceInterface $messageDomainService = null;
+    private ?TelegramBotDomainServiceInterface $telegramBotDomainService = null;
+    private ?ChatDomainServiceInterface $chatDomainService = null;
 
     public function __construct(
         private readonly TelegramBotInterface $bot,
@@ -42,32 +49,52 @@ class TelegramApiManager implements TelegramApiManagerInterface
 
     public function getMe(): UserInterface
     {
-        return $this->telegramBotDomainServiceFactory->create($this->bot, $this->host)->getMe();
+        if (null === $this->telegramBotDomainService) {
+            $this->telegramBotDomainService = $this->telegramBotDomainServiceFactory->create($this->bot, $this->host);
+        }
+
+        return $this->telegramBotDomainService->getMe();
     }
 
     public function logOut(): bool
     {
-        return $this->telegramBotDomainServiceFactory->create($this->bot, $this->host)->logOut();
+        if (null === $this->telegramBotDomainService) {
+            $this->telegramBotDomainService = $this->telegramBotDomainServiceFactory->create($this->bot, $this->host);
+        }
+
+        return $this->telegramBotDomainService->logOut();
     }
 
     public function close(): bool
     {
-        return $this->telegramBotDomainServiceFactory->create($this->bot, $this->host)->close();
+        if (null === $this->telegramBotDomainService) {
+            $this->telegramBotDomainService = $this->telegramBotDomainServiceFactory->create($this->bot, $this->host);
+        }
+
+        return $this->telegramBotDomainService->close();
     }
 
     public function sendMessage(MessageArgumentInterface $argument): MessageInterface
     {
-        return $this->messageDomainServiceFactory->create(
-            $this->bot,
-            $this->host,
-        )->sendMessage($argument);
+        if (null === $this->messageDomainService) {
+            $this->messageDomainService = $this->messageDomainServiceFactory->create(
+                $this->bot,
+                $this->host,
+            );
+        }
+
+        return $this->messageDomainService->sendMessage($argument);
     }
 
     public function getChat(ChatIdArgumentInterface $argument): ChatInterface
     {
-        return $this->chatDomainServiceFactory->create(
-            $this->bot,
-            $this->host,
-        )->getChat($argument);
+        if (null === $this->chatDomainService) {
+            $this->chatDomainService = $this->chatDomainServiceFactory->create(
+                $this->bot,
+                $this->host,
+            );
+        }
+
+        return $this->chatDomainService->getChat($argument);
     }
 }
